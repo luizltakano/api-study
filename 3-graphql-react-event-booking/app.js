544b7dc023a4ac3fa532ffb146e85e3aa866ca0d
Luiz F Takano <luizfltakano@gmail.com>
2023-02-10 22:03:37 +0100

    project 3 implemented user model

--- 3-graphql-react-event-booking/app.js
+++ 3-graphql-react-event-booking/app.js
@@ -3,37 +3,41 @@ const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
 const mongoose = require("mongoose");
 
-const Event = require("./models/events");
+//Require bcrypt and define saltRounds
+const bcrypt = require("bcrypt");
+const saltRounds = 12;
 
+// Require the Event model from the './models/events' module
+const Event = require("./models/events");
+const User = require("./models/users");
+
+// Create an instance of Express
 const app = express();
 
+// Enable JSON and URL-encoded body parsing middleware in Express
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const connectionParams = {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-};
-
-mongoose
-	.connect(
-		`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.eepxz.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
-		connectionParams
-	)
-	.then(
-		app.listen("3000", () => {
-			console.log("Server active on port 3000");
-		})
-	)
-	.catch((err) => console.log(err));
-
+// use the "/graphql" endpoint with graphqlHTTP middleware
 app.use(
 	"/graphql",
 	graphqlHTTP({
+		// define the schema for the GraphQL API
 		schema: buildSchema(`
-			
+
+			type User {
+				_id: ID!
+				email: String!
+				password: String
+			}
+
+			input UserInput {
+				email: String!
+				password: String!
+			}
+
 			type Event {
-				_id: String!
+				_id: ID!
 				title: String!
 				description: String!
 				price: Float!
@@ -51,6 +55,7 @@ app.use(
 			}
 
 			type RootMutation {
+				createUser(userInput: UserInput!): User
 				createEvent(eventInput: EventInput!): Event
 			}
 
@@ -59,6 +64,7 @@ app.use(
 				mutation: RootMutation
 			}
 		`),
+		// provide the root value for resolving the API
 		rootValue: {
 			event: () => {
 				return Event.find()
@@ -68,10 +74,12 @@ app.use(
 						});
 					})
 					.catch((err) => {
+						// throw the error if any
 						throw err;
 					});
 			},
 			createEvent: (args) => {
+				// create a new event using the input arguments
 				const event = new Event({
 					title: args.eventInput.title,
 					description: args.eventInput.description,
@@ -84,10 +92,55 @@ app.use(
 						return { ...result._doc, _id: event.id };
 					})
 					.catch((err) => {
+						// throw the error if any
+						throw err;
+					});
+			},
+			createUser: (args) => {
+				const email = args.userInput.email;
+				return User.findOne({ email: email })
+					.then((user) => {
+						if (user) {
+							throw new Error("Email already exists.");
+						}
+						return bcrypt
+							.hash(args.userInput.password, saltRounds)
+							.then((encryptedPassword) => {
+								const user = new User({
+									email: email,
+									password: encryptedPassword,
+								});
+								return user.save().then((user) => {
+									return { ...user._doc, password: null, _id: user.id };
+								});
+							});
+					})
+					.catch((err) => {
 						throw err;
 					});
 			},
 		},
+		// use the GraphiQL UI for testing the API
 		graphiql: true,
 	})
 );
+
+// Connection parameters for MongoDB
+const connectionParams = {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+};
+
+// Connect to MongoDB with mongoose
+mongoose
+	.connect(
+		// Use environmental variables for the connection URL, username and password
+		`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.eepxz.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
+		connectionParams
+	)
+	.then(
+		app.listen("3000", () => {
+			console.log("Server active on port 3000");
+		})
+	)
+	.catch((err) => console.log(err));
--- 3-graphql-react-event-booking/models/events.js
+++ 3-graphql-react-event-booking/models/events.js
@@ -1,7 +1,10 @@
+// Import mongoose library
 const mongoose = require("mongoose");
 
+// Get access to mongoose's Schema constructor
 const Schema = mongoose.Schema;
 
+// Define event schema with required fields
 const eventSchema = Schema({
 	title: {
 		type: String,
@@ -19,6 +22,11 @@ const eventSchema = Schema({
 		type: Date,
 		required: true,
 	},
+	creator: {
+		type: Schema.Types.ObjectId,
+		ref: "User",
+	},
 });
 
+// Export the event model with the defined schema
 module.exports = mongoose.model("Event", eventSchema);
