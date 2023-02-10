19f7ab2be44972489eb1f579573eb08dbe74349d
Luiz F Takano <luizfltakano@gmail.com>
2023-02-10 20:41:15 +0100

    project 3 mongoose implementation

--- 3-graphql-react-event-booking/app.js
+++ 3-graphql-react-event-booking/app.js
@@ -1,23 +1,57 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
+
+const Event = require("./models/events");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const connectionParams = {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+};
+
+mongoose
+	.connect(
+		`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.eepxz.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
+		connectionParams
+	)
+	.then(
+		app.listen("3000", () => {
+			console.log("Server active on port 3000");
+		})
+	)
+	.catch((err) => console.log(err));
+
 app.use(
 	"/graphql",
 	graphqlHTTP({
 		schema: buildSchema(`
+			
+			type Event {
+				_id: String!
+				title: String!
+				description: String!
+				price: Float!
+				date: String!
+			}
+
+			input EventInput {
+				title: String!
+				description: String!
+				price: Float!
+			}
 
 			type RootQuery {
-				event: [String!]!
+				event: [Event!]!
 			}
 
 			type RootMutation {
-				createEvent(name: String): String
+				createEvent(eventInput: EventInput!): Event
 			}
 
 			schema {
@@ -27,17 +61,33 @@ app.use(
 		`),
 		rootValue: {
 			event: () => {
-				return ["This is amazing", "This is not so amazing"];
+				return Event.find()
+					.then((events) => {
+						return events.map((event) => {
+							return { ...event._doc, _id: event.id };
+						});
+					})
+					.catch((err) => {
+						throw err;
+					});
 			},
 			createEvent: (args) => {
-				const eventName = args.name;
-				return eventName;
+				const event = new Event({
+					title: args.eventInput.title,
+					description: args.eventInput.description,
+					price: +args.eventInput.price,
+					date: new Date().toString(),
+				});
+				return event
+					.save()
+					.then((result) => {
+						return { ...result._doc, _id: event.id };
+					})
+					.catch((err) => {
+						throw err;
+					});
 			},
 		},
 		graphiql: true,
 	})
 );
-
-app.listen("3000", () => {
-	console.log("Server active on port 3000");
-});
